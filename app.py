a4b26995a909db1425f0d0d1a607bc7b1175bdd7
25f1001984-Manish <25f1001984@ds.study.iitm.ac.in>
2026-07-04 17:51:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+import redis
+import os
+
+app = FastAPI()
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = 6379
+
+r = redis.Redis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    decode_responses=True
+)
+
+
+@app.post("/hit/{key}")
+def hit(key: str):
+    count = r.incr(key)
+    return {
+        "key": key,
+        "count": count
+    }
+
+
+@app.get("/count/{key}")
+def count(key: str):
+    value = r.get(key)
+
+    if value is None:
+        value = 0
+
+    return {
+        "key": key,
+        "count": int(value)
+    }
+
+
+@app.get("/healthz")
+def health():
+
+    try:
+        r.ping()
+        return {
+            "status": "ok",
+            "redis": "up"
+        }
+    except:
+        return {
+            "status": "error",
+            "redis": "down"
+        }
